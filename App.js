5c633a7d696e908c59cdc417452c260f74e05fdb
10harshith <harshithburla10@gmail.com>
2025-10-03 19:01:11 +0530

    importing the react
    Removed the CDN links and manually installed react into our local system

--- App.js
+++ App.js
@@ -1,23 +1,12 @@
-{
-  /* <div>
-    <div>
-        <h1>I am H1 tag</h1>
-    </div>
-</div> */
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
+const parent = React.createElement("div", { id: "parent" },
+  React.createElement("div", { id: "child" },
     React.createElement("h1", {}, "I am H1 tag"),
     React.createElement("h2", {}, "I am H2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am H1 tag"),
-    React.createElement("h2", {}, "I am H2 tag"),
-  ]),
-]);
+  ));
 
 console.log(parent);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
